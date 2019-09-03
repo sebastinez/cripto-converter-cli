103b2e2ef2783a18c8fd3ee3aaabdfb1fa4265ce
Sebastinez <sebastinez@me.com>
2019-09-03 17:19:02 -0300

    Improved Documentation, Edge Cases and Look

---
+++ src/asciiAndBytes32.js
@@ -0,0 +1,74 @@
+const utf8 = require("utf8");
+const { stripHexPrefix, isHexStrict, rightPad } = require("../src/utils");
+const chalk = require("chalk");
+
+const asciitobytes32 = (value, length = 32) => {
+  let hex = "";
+
+  for (let i = 0; i < value.length; i += 1) {
+    const code = value.charCodeAt(i);
+    const n = code.toString(16);
+    hex += n.length < 2 ? `0${n}` : n;
+  }
+  const hexString = `0x${rightPad(hex, length * 2)}`;
+
+  if (hexString.length > 66) {
+    console.log(
+      chalk.underline("\nError:\n\n") + `String: [${value}]\nToo long to be converted, does not fit in 32 bytes`
+    );
+    return;
+  }
+  console.log(
+    chalk.underline("\nConverted ASCII string to Bytes32 String\n\n") +
+      `Input: ${value}\nOutput: 0x${rightPad(hex, length * 2)}`
+  );
+  return;
+};
+
+const bytes32ToAscii = hex => {
+  let input = hex;
+  if (hex.length !== 66) {
+    console.log(
+      chalk.underline("\nError:\n") +
+        "The string is not the correct length, should be 66 digits long, including 0x prefix"
+    );
+    return;
+  }
+  if (!hex.startsWith("0x")) {
+    console.log(chalk.underline("\nError:\n") + "The string should start with 0x");
+    return;
+  }
+  if (!isHexStrict(hex)) {
+    console.log(chalk.underline("\nError:\n") + `The parameter "${hex}" must be a valid HEX string.`);
+    return;
+  }
+
+  let string = "";
+  let code = 0;
+  hex = stripHexPrefix(hex);
+
+  // remove 00 padding from either side
+  hex = hex.replace(/^(?:00)*/, "");
+  hex = hex
+    .split("")
+    .reverse()
+    .join("");
+  hex = hex.replace(/^(?:00)*/, "");
+  hex = hex
+    .split("")
+    .reverse()
+    .join("");
+
+  const l = hex.length;
+
+  for (let i = 0; i < l; i += 2) {
+    code = parseInt(hex.substr(i, 2), 16);
+    string += String.fromCharCode(code);
+  }
+  console.log(
+    chalk.underline("\nConverted Bytes32 string to ASCII String\n\n") +
+      `Input: ${input}\nOutput: ${utf8.decode(string)}`
+  );
+};
+
+module.exports = { asciitobytes32, bytes32ToAscii };
--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,17 @@
-const isNumber = require('lodash/isNumber');
-const isString = require('lodash/isString');
+const stripHexPrefix = string => {
+  return string.startsWith("0x") || string.startsWith("0X") ? string.slice(2) : string;
+};
+const isHexStrict = hex => {
+  return (typeof hex == "string" || typeof hex == "number") && /^(-)?0x[0-9a-f]*$/i.test(hex);
+};
 
-function stripHexPrefix(string) {
-  return string.startsWith('0x') || string.startsWith('0X') ? string.slice(2) : string;
-}
-function isHexStrict(hex) {
-  return (isString(hex) || isNumber(hex)) && /^(-)?0x[0-9a-f]*$/i.test(hex);
-}
-
-function rightPad(string, chars, sign) {
-  const hasPrefix = /^0x/i.test(string) || typeof string === 'number';
-  const hexString = string.toString(16).replace(/^0x/i, '');
+const rightPad = (string, chars, sign) => {
+  const hasPrefix = /^0x/i.test(string) || typeof string === "number";
+  const hexString = string.toString(16).replace(/^0x/i, "");
 
   const padding = chars - hexString.length + 1 >= 0 ? chars - hexString.length + 1 : 0;
 
-  return (hasPrefix ? '0x' : '') + hexString + new Array(padding).join(sign || '0');
-}
+  return (hasPrefix ? "0x" : "") + hexString + new Array(padding).join(sign || "0");
+};
 
 module.exports = { stripHexPrefix, isHexStrict, rightPad };

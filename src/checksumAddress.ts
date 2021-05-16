72b1f6987f0b4938b92d1097e3bdc988ec698bce
sebastinez <smartinez@nuclearis.com>
2021-05-15 21:57:26 -0300

    feat: change eth-lib for ethereum-cryptography

--- src/checksumAddress.ts
+++ src/checksumAddress.ts
@@ -1,21 +1,24 @@
-import Hash from "eth-lib/lib/hash";
+import { keccak256 } from "ethereum-cryptography/keccak";
 import chalk from "chalk";
 import { stripHexPrefix } from "./utils";
 
-const toChecksumAddress = (address: string, chainId:null|number = null) => {
+const toChecksumAddress = (address: string, chainId: null | number = null) => {
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-    console.log(chalk.underline("\nError:\n\n") + `Given address "${address}" is not a valid Ethereum address.`);
+    console.log(
+      chalk.underline("\nError:\n\n") +
+        `Given address "${address}" is not a valid Ethereum address.`
+    );
     return;
   }
 
   const stripAddress = stripHexPrefix(address).toLowerCase();
   const prefix = chainId != null ? `${chainId.toString()}0x` : "";
-  const keccakHash = Hash.keccak256(prefix + stripAddress)
+  const keccakHash = keccak256(Buffer.from(prefix + stripAddress))
+    .toString()
     .replace(/^0x/i, "");
   let checksumAddress = "0x";
 
   for (let i = 0; i < stripAddress.length; i += 1)
-
     checksumAddress +=
       parseInt(keccakHash[i], 16) >= 8
         ? stripAddress[i].toUpperCase()
